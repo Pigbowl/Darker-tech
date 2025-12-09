9977c74c6a657ec3be34979468385c577dbab201
Darker <song-jiawei@outlook.com>
2025-12-09 23:47:34 +0800

    Auto commit - 07a2384 Auto commit - a8bd1be Auto commit - 8dad459 Auto commit - d7c835e Auto commit - 02c1293 Auto commit - 27bf86e Auto commit - a8040d1 Auto commit - 3b654f6 Auto commit - bb53a49 Auto commit - 7837724 Auto commit - f41a9a7 最后试一次

--- js/version_control.js
+++ js/version_control.js
@@ -1,15 +1,15 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-09 23:37:26
+// 生成时间: 2025-12-09 23:47:14
 
 // 全局版本内容变量
 let version_content = {
-  "version": "BETA-2512-1",
+  "version": "BETA-2512-2",
   "major_version": "BETA",
   "minor_year": 25,
   "minor_month": 12,
-  "temp_version": 1,
-  "release_date": "2025-12-09 23:37:26",
+  "temp_version": 2,
+  "release_date": "2025-12-09 23:47:14",
   "changes": [
     {
       "Belong_Feature": "功能星球",
