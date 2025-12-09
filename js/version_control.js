55ac59c1f0214dc784137a887474e3776a02c7d3
Darker <song-jiawei@outlook.com>
2025-12-10 00:11:04 +0800

    Auto commit - dfc23e8 Auto commit - d07f6d9 Auto commit - 5711c20 Auto commit - b52c03d Auto commit - 9977c74 Auto commit - 07a2384 Auto commit - a8bd1be Auto commit - 8dad459 Auto commit - d7c835e Auto commit - 02c1293 Auto commit - 27bf86e Auto commit - a8040d1 Auto commit - 3b654f6 Auto commit - bb53a49 Auto commit - 7837724 Auto commit - f41a9a7 最后试一次

--- js/version_control.js
+++ js/version_control.js
@@ -1,15 +1,15 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-09 23:57:41
+// 生成时间: 2025-12-10 00:10:42
 
 // 全局版本内容变量
 let version_content = {
-  "version": "ALPHA-2512-1",
+  "version": "ALPHA-2512-2",
   "major_version": "ALPHA",
   "minor_year": 25,
   "minor_month": 12,
-  "temp_version": 1,
-  "release_date": "2025-12-09 23:57:41",
+  "temp_version": 2,
+  "release_date": "2025-12-10 00:10:42",
   "changes": [
     {
       "Belong_Feature": "功能星球",
@@ -17,8 +17,8 @@ let version_content = {
       "Content_Type": "creation"
     },
     {
-      "Belong_Feature": "传感器配置器",
-      "Content": "发布“传感器配置器”功能，自行定义，快速绘制你的传感器配置图",
+      "Belong_Feature": "硬件星球",
+      "Content": "发布\"硬件星球”板块业务，提供ADAS用户功能知识信息",
       "Content_Type": "creation"
     }
   ]
