07a238448dc7e92cab20eb565b356edcae73dee8
Darker <song-jiawei@outlook.com>
2025-12-09 23:37:38 +0800

    Auto commit - a8bd1be Auto commit - 8dad459 Auto commit - d7c835e Auto commit - 02c1293 Auto commit - 27bf86e Auto commit - a8040d1 Auto commit - 3b654f6 Auto commit - bb53a49 Auto commit - 7837724 Auto commit - f41a9a7 最后试一次

--- js/version_control.js
+++ js/version_control.js
@@ -1,45 +1,20 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-09 23:32:20
+// 生成时间: 2025-12-09 23:37:26
 
 // 全局版本内容变量
 let version_content = {
-  "version": "ALPHA-2512-1",
-  "major_version": "ALPHA",
+  "version": "BETA-2512-1",
+  "major_version": "BETA",
   "minor_year": 25,
   "minor_month": 12,
   "temp_version": 1,
-  "release_date": "2025-12-09 23:32:20",
+  "release_date": "2025-12-09 23:37:26",
   "changes": [
     {
       "Belong_Feature": "功能星球",
       "Content": "发布\"功能星球”板块业务，提供ADAS用户功能知识信息",
       "Content_Type": "creation"
-    },
-    {
-      "Belong_Feature": "硬件星球",
-      "Content": "发布\"硬件星球”板块业务，提供ADAS用户功能知识信息",
-      "Content_Type": "creation"
-    },
-    {
-      "Belong_Feature": "知识网络",
-      "Content": "发布“知识网络”功能，通过交互式网络图提供ADAS学习的脉络和方法",
-      "Content_Type": "creation"
-    },
-    {
-      "Belong_Feature": "传感器星球",
-      "Content": "发布“传感器星球”功能",
-      "Content_Type": "creation"
-    },
-    {
-      "Belong_Feature": "传感器配置器",
-      "Content": "发布“传感器配置器”功能，自行定义，快速绘制你的传感器配置图",
-      "Content_Type": "creation"
-    },
-    {
-      "Belong_Feature": "环境生成器",
-      "Content": "3D预览功能无法使用",
-      "Content_Type": "bug_fix"
     }
   ]
 };
