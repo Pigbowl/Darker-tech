5d20c8c6d534f584a9e1ac9db0c57e94317c3a90
Darker <song-jiawei@outlook.com>
2025-12-10 09:48:38 +0800

    Auto commit - 4064c85 Auto commit - 989b53a Auto commit - db6c7bd Auto commit - c4366cb Auto commit - 55ac59c Auto commit - dfc23e8 Auto commit - d07f6d9 Auto commit - 5711c20 Auto commit - b52c03d Auto commit - 9977c74 Auto commit - 07a2384 Auto commit - a8bd1be Auto commit - 8dad459 Auto commit - d7c835e Auto commit - 02c1293 Auto commit - 27bf86e Auto commit - a8040d1 Auto commit - 3b654f6 Auto commit - bb53a49 Auto commit - 7837724 Auto commit - f41a9a7 最后试一次

--- js/version_control.js
+++ js/version_control.js
@@ -1,24 +1,19 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-10 00:56:28
+// 生成时间: 2025-12-10 09:48:21
 
 // 全局版本内容变量
 let version_content = {
-  "version": "BETA-2512-3",
+  "version": "BETA-2512-4",
   "major_version": "BETA",
   "minor_year": 25,
   "minor_month": 12,
-  "temp_version": 3,
-  "release_date": "2025-12-10 00:56:28",
+  "temp_version": 4,
+  "release_date": "2025-12-10 09:48:21",
   "changes": [
     {
       "Belong_Feature": "功能星球",
       "Content": "发布\"功能星球”板块业务，提供ADAS用户功能知识信息",
-      "Content_Type": "creation"
-    },
-    {
-      "Belong_Feature": "硬件星球",
-      "Content": "发布\"硬件星球”板块业务，提供ADAS用户功能知识信息",
       "Content_Type": "creation"
     }
   ]
