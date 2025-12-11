d548279676575da581ba09a1e6f78e1b3270ce98
Darker <song-jiawei@outlook.com>
2025-12-11 14:57:00 +0800

    Alpha-25-12-2

--- js/version_control.js
+++ js/version_control.js
@@ -1,15 +1,15 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-11 00:30:12
+// 生成时间: 2025-12-11 14:56:44
 
 // 全局版本内容变量
 let version_content = {
-  "version": "BETA-2512-1",
-  "major_version": "BETA",
+  "version": "Alpha-2512-2",
+  "major_version": "Alpha",
   "minor_year": 25,
   "minor_month": 12,
-  "temp_version": 1,
-  "release_date": "2025-12-11 00:30:12",
+  "temp_version": 2,
+  "release_date": "2025-12-11 14:56:44",
   "changes": [
     {
       "Belong_Feature": "功能星球",
