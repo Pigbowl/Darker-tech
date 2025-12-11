e6a6d4d73beb195a1782e1ef59332ef82302107c
Darker <song-jiawei@outlook.com>
2025-12-11 16:23:33 +0800

    Alpha-25-12-5

--- js/version_control.js
+++ js/version_control.js
@@ -1,19 +1,19 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-11 16:08:17
+// 生成时间: 2025-12-11 16:23:18
 
 // 全局版本内容变量
 let version_content = {
-  "version": "Alpha-2512-4",
+  "version": "Alpha-2512-5",
   "major_version": "Alpha",
   "minor_year": 25,
   "minor_month": 12,
-  "temp_version": 4,
-  "release_date": "2025-12-11 16:08:17",
+  "temp_version": 5,
+  "release_date": "2025-12-11 16:23:18",
   "changes": [
     {
-      "Belong_Feature": "功能星球",
-      "Content": "发布\"功能星球”板块业务，提供ADAS用户功能知识信息",
+      "Belong_Feature": "环境生成器",
+      "Content": "发布“环境生成器”功能",
       "Content_Type": "creation"
     }
   ]
