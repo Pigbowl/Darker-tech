daffb1e4729c76405ee6d1b0e19239394c1badf5
Darker <song-jiawei@outlook.com>
2025-12-12 21:12:07 +0800

    Beta-25-12-8

--- js/version_control.js
+++ js/version_control.js
@@ -1,6 +1,6 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-12 16:28:11
+// 生成时间: 2025-12-12 21:11:52
 
 // 全局版本内容变量
 let version_content = {
@@ -9,7 +9,7 @@ let version_content = {
   "minor_year": 25,
   "minor_month": 12,
   "temp_version": 8,
-  "release_date": "2025-12-12 16:28:11",
+  "release_date": "2025-12-12 21:11:52",
   "changes": [
     {
       "Belong_Feature": "产品配置器",
