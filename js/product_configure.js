90bd11fdf5c6d3b17da92b7d74550a1b42862179
Darker <song-jiawei@outlook.com>
2025-12-14 22:13:49 +0800

    aaaa-Quick_Fix

--- js/product_configure.js
+++ js/product_configure.js
@@ -1,6 +1,6 @@
 
 // 产品配置信息 - 自动生成
-// 生成时间: 2025-12-14 22:11:43
+// 生成时间: 2025-12-14 22:13:45
 
 // 全局产品配置变量
 let products_config = {
@@ -324,7 +324,7 @@ let products_config = {
       }
     ]
   },
-  "version": "My little duck fixation-Quick_Fix",
+  "version": "aaaa-Quick_Fix",
   "status": [
     "新产品上线",
     "未上线",
