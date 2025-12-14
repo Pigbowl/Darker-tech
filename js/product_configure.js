e886cb71e36df5ec04efcb1972cf60ac292ff251
Darker <song-jiawei@outlook.com>
2025-12-14 22:09:50 +0800

    darkduck fixation-Quick_Fix

--- js/product_configure.js
+++ js/product_configure.js
@@ -1,6 +1,6 @@
 
 // 产品配置信息 - 自动生成
-// 生成时间: 2025-12-14 21:59:40
+// 生成时间: 2025-12-14 22:09:47
 
 // 全局产品配置变量
 let products_config = {
@@ -324,7 +324,7 @@ let products_config = {
       }
     ]
   },
-  "version": "Duck Fix-Quick_Fix",
+  "version": "darkduck fixation-Quick_Fix",
   "status": [
     "新产品上线",
     "未上线",
