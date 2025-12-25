6b2e38527b2004652b015f3354965f3be239c635
Darker <song-jiawei@outlook.com>
2025-12-25 17:01:24 +0800

    Beta-25-12-9-Official_Release

--- js/product_configure.js
+++ js/product_configure.js
@@ -1,6 +1,6 @@
 
 // 产品配置信息 - 自动生成
-// 生成时间: 2025-12-25 15:34:51
+// 生成时间: 2025-12-25 17:00:45
 
 // 全局产品配置变量
 let products_config = {
@@ -340,6 +340,6 @@ let products_config = {
       }
     ]
   },
-  "version": "pre-test-1225-Quick_Fix",
+  "version": "Beta-25-12-9-Official_Release",
   "status": []
 };
--- js/version_control.js
+++ js/version_control.js
@@ -1,20 +1,55 @@
 
 // 版本更新内容 - 自动生成
-// 生成时间: 2025-12-12 21:11:52
+// 生成时间: 2025-12-25 17:00:37
 
 // 全局版本内容变量
 let version_content = {
-  "version": "Beta-2512-8",
+  "version": "Beta-2512-9",
   "major_version": "Beta",
   "minor_year": 25,
   "minor_month": 12,
-  "temp_version": 8,
-  "release_date": "2025-12-12 21:11:52",
+  "temp_version": 9,
+  "release_date": "2025-12-25 17:00:37",
   "changes": [
     {
-      "Belong_Feature": "产品配置器",
-      "Content": "lllll",
+      "Belong_Feature": "通用信息",
+      "Content": "用户订阅网站后，无用户信息",
       "Content_Type": "bug_fix"
+    },
+    {
+      "Belong_Feature": "通用信息",
+      "Content": "新增初始页面是达客介绍动画，提高达客鸭的认知度",
+      "Content_Type": "creation"
+    },
+    {
+      "Belong_Feature": "通用信息",
+      "Content": "新增达克助手鸭拖拽功能，允许将智能助手拖拽到任何位置",
+      "Content_Type": "creation"
+    },
+    {
+      "Belong_Feature": "通用信息",
+      "Content": "新增用户注册和个人空间，允许用户记录在网页上的信息，累计和行为",
+      "Content_Type": "creation"
+    },
+    {
+      "Belong_Feature": "架构星球",
+      "Content": "新上线产品架构星球",
+      "Content_Type": "creation"
+    },
+    {
+      "Belong_Feature": "通用信息",
+      "Content": "新增用户页面，点亮星球功能，帮助用户知晓还有多少“星球”可以探索",
+      "Content_Type": "creation"
+    },
+    {
+      "Belong_Feature": "通用信息",
+      "Content": "发布\"开始旅程\"功能-初版",
+      "Content_Type": "creation"
+    },
+    {
+      "Belong_Feature": "知识网络",
+      "Content": "发布“知识网络”功能",
+      "Content_Type": "creation"
     }
   ]
 };
